94709f74dc8877b60b3aa46fe5781b7ad6f23b6a
Surya Bumantara <bumantara.surya2213@gmail.com>
2022-05-25 20:30:26 +0700

    + add Laporan Transaksi yang terjadi dalam 1 bulan untuk penjual

--- app/src/main/java/com/example/proyek_mobcomp/SellerPendapatanActivity.java
+++ app/src/main/java/com/example/proyek_mobcomp/SellerPendapatanActivity.java
@@ -1,12 +1,34 @@
 package com.example.proyek_mobcomp;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -17,27 +39,33 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.proyek_mobcomp.classFolder.cDetailPurchase;
 import com.example.proyek_mobcomp.classFolder.cHeaderPurchase;
-import com.example.proyek_mobcomp.classFolder.cProduct;
 import com.example.proyek_mobcomp.databinding.ActivitySellerPendapatanBinding;
 import com.example.proyek_mobcomp.recyclerviewFolder.RecyclerAdapterSellerPendapatan;
-import com.example.proyek_mobcomp.recyclerviewFolder.RecyclerAdapterSellerTransaksi;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SellerPendapatanActivity extends AppCompatActivity {
-
+    private static final int PERMISSION_REQUEST_CODE = 200;
     /*
 
         Created at : 28 April 2022 untuk menampilkan data pendapatan seller
 
      */
-
+    int pageHeight = 1120;
+    int pagewidth = 792;
+    Bitmap bmp, scaledbmp;
     ActivitySellerPendapatanBinding binding;
 
     RecyclerAdapterSellerPendapatan recyclerAdapterSellerPendapatan;
@@ -60,6 +88,15 @@ public class SellerPendapatanActivity extends AppCompatActivity {
         resetAllFilter();
 
         getDataPendapatan();
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
+        scaledbmp = Bitmap.createScaledBitmap(bmp, 140, 140, false);
+        // below code is used for
+        // checking our permissions.
+        if (checkPermission()) {
+            //Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermission();
+        }
 
         binding.imageButtonBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +104,157 @@ public class SellerPendapatanActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        binding.btnLaporanTransaksiSeller.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                generatePDF();
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void generatePDF() {
+        // creating an object variable
+        // for our PDF document.
+        PdfDocument pdfDocument = new PdfDocument();
+
+        // berapa banyak page yg akan dicetak berdasarkan isi
+        int datacount = listHTrans.size();
+        int pageNumber = (int) Math.ceil(listHTrans.size() / 15.0);
+        int idxArrWritten = 0;
+        int idxNumber = 1;
+
+        for (int i = 0; i < pageNumber; i++){
+            Paint paint = new Paint();
+            Paint title = new Paint();
+
+            PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(pagewidth, pageHeight, 2).create();
+            PdfDocument.Page myPage = pdfDocument.startPage(mypageInfo);
+            Canvas canvas = myPage.getCanvas();
+
+            // HEADER PDF
+            canvas.drawBitmap(scaledbmp, 56, 40, paint);
+            title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+            title.setTextSize(18);
+            title.setColor(ContextCompat.getColor(this, R.color.black));
+
+            canvas.drawText("Laporan Penjualan", 209, 85, title);
+            canvas.drawText("Kepada Seller dengan nama toko " + SellerActivity.login, 209, 110, title);
+
+            SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+            SimpleDateFormat output = new SimpleDateFormat("dd MMMM yyyy");
+
+            Calendar calendar = Calendar.getInstance();
+
+            Date d = null;
+            try
+            {
+                d = input.parse(calendar.getTime().toString());
+            }
+            catch (ParseException e)
+            {
+                e.printStackTrace();
+            }
+            String formatted = output.format(calendar.getTime());
+            canvas.drawText("Per " + formatted+"", 209, 135, title);
+
+            // CONTENT PDF
+            title.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+            title.setColor(ContextCompat.getColor(this, R.color.black));
+            title.setTextSize(14);
+
+            // header table
+            canvas.drawLine(50, 200, 742, 200, paint);
+            canvas.drawText("No. ", 100, 219, title);
+            canvas.drawText("ID Transaksi ", 200, 219, title);
+            canvas.drawText("Tanggal ", 375, 219, title);
+            canvas.drawText("Grandotal", 575, 219, title);
+            canvas.drawLine(50, 230, 742, 230, paint);
+
+
+            int pagecount = datacount;
+            if (pagecount > 15) pagecount = 15;
+            for (int j = 0; j < pagecount; j++){
+                title.setColor(ContextCompat.getColor(this, R.color.black));
+
+
+                input = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+                output = new SimpleDateFormat("dd MMMM yyyy");
+
+                canvas.drawText(idxNumber + "", 100, 219 + (50 * (j+1)), title);
+                canvas.drawText("#" + listHTrans.get(j).getId(), 200, 219 + (50 * (j+1)), title);
+                canvas.drawText(listHTrans.get(j).getTanggal()+"", 375, 219 + (50 * (j+1)), title);
+                canvas.drawText("Rp " + listHTrans.get(j).getGrandtotalInString(), 575, 219 + (50 * (j+1)), title);
+
+                idxArrWritten++;
+                idxNumber++;
+            }
+            Log.d("V", pagecount + "");
+            datacount -= 15;
+            // after adding all attributes to our
+            // PDF file we will be finishing our page.
+            pdfDocument.finishPage(myPage);
+        }
+        SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        SimpleDateFormat output = new SimpleDateFormat("dd MMMM yyyy");
+
+        Calendar calendar = Calendar.getInstance();
+
+        String formatted = output.format(calendar.getTime());
+        File file = new File(Environment.getExternalStorageDirectory(), "Laporan Penjualan - " + formatted+".pdf");
+
+        try {
+            pdfDocument.writeTo(new FileOutputStream(file));
+            Toast.makeText(this, "Laporan Penjualan berhasil diunduh.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        pdfDocument.close();
+    }
+
+    private boolean checkPermission() {
+        // checking of permissions.
+        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        // requesting permissions if not provided.
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+
+                // after requesting permissions we are showing
+                // users a toast message of permission granted.
+                boolean writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if (writeStorage && readStorage) {
+                    Toast.makeText(this, "Permission Granted.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denied.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
+    }
+
+    // Get Extension
+    public String GetFileExtension(Uri uri)
+    {
+        ContentResolver contentResolver=getContentResolver();
+        MimeTypeMap mimeTypeMap=MimeTypeMap.getSingleton();
+
+        // Return file Extension
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
     }
 
     public void resetAllFilter() {
